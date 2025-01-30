4f839332136c56faa633067b6f0be01d4c2e233d
rangarajanpraneeth <rangarajanpraneeth@gmail.com>
2025-01-29 21:10:59 -0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+importScripts(
+   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+workbox.routing.registerRoute(
+   ({ request }) => request.destination === 'image',
+   new workbox.strategies.NetworkFirst()
+);
+
+
+
+
 let currentSubject = null;
 let currentQuestionIndex = 0;
 let totalScore = 0;
